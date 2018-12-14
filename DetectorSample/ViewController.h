547850d97f61621f237eb3b94740d88ea80828e5
luoyingxing <luoyingxing@126.com>
2018-12-14 15:07:14 +0800

    Initial Commit

---
+++ DetectorSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DetectorSample
+//
+//  Created by conwin on 2018/12/14.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DetectorSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DetectorSample
+//
+//  Created by conwin on 2018/12/14.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
