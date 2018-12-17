e0afa2a7b83e39608402700c0b5593a83f17fe38
luoyingxing <luoyingxing@126.com>
2018-12-17 10:47:21 +0800

    update

--- DetectorSample/ViewController.m
+++ DetectorSample/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  iOSFrame
 //
-//  Created by 罗映星 on 2018/10/30.
-//  Copyright © 2018 罗映星. All rights reserved.
+//  Created by luoyingxing on 2018/10/30.
+//  Copyright © 2018 luoyingxing. All rights reserved.
 //
 
 #import "ViewController.h"
