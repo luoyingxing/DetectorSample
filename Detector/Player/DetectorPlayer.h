b5872f473296296973cea5fce6a0e4e4c0b6f39d
luoyingxing <luoyingxing@126.com>
2018-12-21 17:21:38 +0800

    add DetectorPlayer

---
+++ Detector/Player/DetectorPlayer.h
@@ -0,0 +1,13 @@
+//
+//  DetectorPlayer.h
+//  DetectorSample
+//
+//  Created by conwin on 2018/12/21.
+//  Copyright © 2018年 luoyingxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetectorPlayer : NSObject
+
+@end
---
+++ Detector/Player/DetectorPlayer.m
@@ -0,0 +1,13 @@
+//
+//  DetectorPlayer.m
+//  DetectorSample
+//
+//  Created by conwin on 2018/12/21.
+//  Copyright © 2018年 luoyingxing. All rights reserved.
+//
+
+#import "DetectorPlayer.h"
+
+@implementation DetectorPlayer
+
+@end
